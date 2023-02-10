8e019a6be0134da5d505d8a9ce56a3c14279a134
Alex Forsythe <awforsythe@gmail.com>
2023-02-10 18:21:46 -0500

    Add a bare-bones test function to be callable via wasm

---
+++ include/ad_example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cinttypes>
+
+#include "em.h"
+
+int32_t AD_EXPORT ad_example_func(int32_t x);
---
+++ src/ad_example.cpp
@@ -0,0 +1,6 @@
+#include "ad_example.h"
+
+int32_t ad_example_func(int32_t x)
+{
+    return x * 2;
+}
